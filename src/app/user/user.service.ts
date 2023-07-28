1546bb318d2604a66dd8617cf6eaf588049a9405
lucas-de-F <francalucas937@gmail.com>
2023-07-28 15:18:42 -0300

    feat: add

--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-import { User } from '@prisma/client';
 import { PrismaService } from 'prisma/prisma.service';
 import { UserAuthService } from 'src/auth/user_auth/user_auth.service';
 import { v4 as uuidv4 } from 'uuid';
