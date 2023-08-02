972a724dd922c1a49b77e97832b8e0bcd24e075c
lucas-de-F <francalucas937@gmail.com>
2023-08-02 10:33:22 -0300

    feat: validação de login

--- src/auth/login/check-params.ts
+++ src/auth/login/check-params.ts
@@ -5,9 +5,10 @@ import {
   UnauthorizedException,
 } from '@nestjs/common';
 import { UserAuthService } from '../user_auth/user_auth.service';
-import { NextFunction } from 'express';
+import { NextFunction, Request } from 'express';
+const Joi = require('joi');
 
-interface Request {
+interface Requestq extends Request {
   method: 'POST' | 'GET' | 'PUT' | 'DELETE';
   body: {
     email: string;
@@ -17,31 +18,57 @@ interface Request {
 @Injectable()
 export class ValidateLoginMiddleware implements NestMiddleware {
   constructor(private userAuthService: UserAuthService) {}
-  async use(req: Request, res: Response, next: NextFunction) {
-    if (req.method === 'POST') {
+  async use(req: Requestq, res: Response, next: NextFunction) {
+    if (req.method === 'POST' && req.path.includes('register')) {
+      try {
+        const schema = Joi.object({
+          password: Joi.string()
+            .min(6)
+            .pattern(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{2,}$/,
+            )
+            .required(),
+          email: Joi.string().email().lowercase().required(),
+        });
+        await schema.validateAsync(req.body);
+      } catch (e) {
+        throw new BadRequestException({
+          message: `dados inválidos`,
+          errorType: `Email ou senha inválidos`,
+          statusCode: 400,
+          error: 'Bad Request',
+        });
+      }
+    }
+    if (req.method === 'POST' && !req.path.includes('register')) {
+      try {
+        const schema = Joi.object({
+          password: Joi.string()
+            .min(6)
+            .pattern(
+              /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{2,}$/,
+            )
+            .required(),
+          email: Joi.string().email().lowercase().required(),
+        });
+        await schema.validateAsync(req.body);
+      } catch (e) {
+        throw new BadRequestException({
+          message: `dados inválidos`,
+          errorType: `Email ou senha inválidos`,
+          statusCode: 400,
+          error: 'Bad Request',
+        });
+      }
       try {
         const user = await this.userAuthService.getUserAuth(req.body.email);
-        if (user === null) throw Error('Usuário Inválido');
+        if (user === null) throw new Error('Usuário sem pré-cadastro');
         if (user.User === null) throw Error('Usuário Inativo');
       } catch (e) {
         throw new UnauthorizedException({
           message: `${e.message}`,
           errorType: `${e.message}`,
           statusCode: 401,
-          error: 'Bad Request',
-        });
-      }
-      try {
-        const isOnPreRegister = await this.userAuthService.getUserAuth(
-          req.body.email,
-        );
-
-        if (!isOnPreRegister) throw new Error('Usuário sem pré-cadastro');
-      } catch (e) {
-        throw new BadRequestException({
-          message: `dados inválidos`,
-          errorType: `${e.message}`,
-          statusCode: 400,
           error: 'Bad Request',
         });
       }
