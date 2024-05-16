b326299d4764ade706e50dfed499a0ea078edba4
DAmalek <danielmalek101@gmail.com>
2024-05-16 17:12:42 -0300

    fix: retirada de redundancia no editDto

--- src/admin/user/dto/EditUserDto.ts
+++ src/admin/user/dto/EditUserDto.ts
@@ -2,34 +2,8 @@ import { ApiProperty, OmitType } from '@nestjs/swagger';
 import { CreateUserBody } from './CreateUserDto';
 import { IsArray, IsNotEmpty, IsString, MinLength } from 'class-validator';
 
-export class EditUserBody {
+export class EditUserBody extends OmitType(CreateUserBody, ['email'] as const) {
   @ApiProperty()
   @IsNotEmpty()
   id: string;
-
-  @ApiProperty()
-  @IsString()
-  @MinLength(3)
-  @IsNotEmpty()
-  name: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  office_id: string;
-
-  @ApiProperty()
-  @IsString()
-  @IsNotEmpty()
-  rls_id: string;
-
-  @ApiProperty()
-  @IsNotEmpty()
-  @IsArray()
-  projects: string[];
-
-  // @ApiProperty()
-  // profession: string;
-  // @ApiProperty()
-  // description: string;
 }
